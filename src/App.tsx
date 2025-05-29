4d42932b8937c7722a5080cc9e45d59b3b63c3cd
Uday <bhanusharm122@gmail.com>
2025-05-29 23:55:58 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ export default function App() {
         </div>
       )}
       {loadingImage && (
-        <div className="w-full flex justify-center mt-4">
+        <div className="w-full flex justify-center mt-4"> 
           <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-600"></div>
         </div>
       )}
